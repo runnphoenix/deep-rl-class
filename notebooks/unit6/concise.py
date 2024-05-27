5e67c469791ab9df521ad78a202fd89d8c41a75d
Chris Zhang <runnphoenix@gmail.com>
2024-05-27 12:52:02 +0800

    added concise python script in unit6

---
+++ notebooks/unit6/concise.py
@@ -0,0 +1,71 @@
+import os
+
+import gymnasium as gym
+import panda_gym
+
+from stable_baselines3 import A2C
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.monitor import Monitor
+from pyvirtualdisplay import Display
+
+virtual_display = Display(visible=1, size=(1400, 900))
+virtual_display.start()
+
+env_id = "PandaReachDense-v3"
+
+# Create the env
+env = gym.make(env_id)
+
+# Get the state space and action space
+s_size = env.observation_space.shape
+a_size = env.action_space
+
+print("_____OBSERVATION SPACE_____ \n")
+print("The State Space is: ", s_size)
+print("Sample observation", env.observation_space.sample()) # Get a random observation
+print("\n _____ACTION SPACE_____ \n")
+print("The Action Space is: ", a_size)
+print("Action Space Sample", env.action_space.sample()) # Take a random action
+
+
+# Create the env
+env = make_vec_env(env_id, n_envs=4)
+env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.)
+
+model = A2C(policy = "MultiInputPolicy",
+            env = env,
+            verbose=1)
+'''
+model.learn(1_000_000)
+
+# Save the model and  VecNormalize statistics when saving the agent
+model.save("a2c-PandaReachDense-v3")
+env.save("vec_normalize.pkl")
+
+'''
+
+# Load the saved statistics
+eval_env = DummyVecEnv([lambda: gym.make("PandaReachDense-v3")])
+eval_env = VecNormalize.load("vec_normalize.pkl", eval_env)
+
+# We need to override the render_mode
+eval_env.render_mode = "rgb_array"
+
+#  do not update them at test time
+eval_env.training = False
+# reward normalization is not needed at test time
+eval_env.norm_reward = False
+
+# Load the agent
+model = A2C.load("a2c-PandaReachDense-v3")
+mean_reward, std_reward = evaluate_policy(model, eval_env)
+print(f"Mean reward = {mean_reward:.2f} +/- {std_reward:.2f}")
+
+# Show
+obs = eval_env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = eval_env.step(action)
+    eval_env.render("human")
