bcd604295bbd9f02c67c4422c265a2e1b2d2277e
Chris Zhang <runnphoenix@gmail.com>
2024-11-22 17:56:04 +0800

    fixing problem: the pg lunar lander failed to learn stop

--- notebooks/unit1/lunar-lander-pg.py
+++ notebooks/unit1/lunar-lander-pg.py
@@ -21,11 +21,12 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 state_len = 8
 action_len = 4
 
-LearningRate = 1e-4
-MaxEpisodeSteps = 300
-NumEpisodes = 5000
+LearningRate = 2e-3
+NumBatches = 300
+EpisodesPerBatch = 100
+Gamma = 0.99
 
-class DQN(nn.Module):
+class PG(nn.Module):
     def __init__(self, dims):
         super().__init__()
   	
@@ -40,19 +41,20 @@ class DQN(nn.Module):
         return self.net(x)
 
 # Create memory and Q-Net
-dqn = DQN([8,128,128,4]).to(device)
+pg = PG([8,64,128,32,4]).to(device)
 
-def get_policy(q_net, state):
-    return Categorical(logits = q_net(torch.FloatTensor(state).to(device)))
+def get_policy(model, state):
+    prob = model(torch.FloatTensor(state).to(device))
+    return Categorical(logits = prob)
 
-def select_action(state, q_net):
-    action = get_policy(q_net, state).sample().item()
+def select_action(state, model):
+    action = get_policy(model, state).sample().item()
     return action
 
-def calculate_loss(q_net, actions, states, rewards):
+def calculate_loss(model, actions, states, rewards):
     rewards  = torch.FloatTensor(rewards).to(device)
     actions = torch.FloatTensor(actions).to(device)
-    probs = get_policy(q_net, states).log_prob(actions)
+    probs = get_policy(model, states).log_prob(actions)
     loss = -probs * rewards
 
     return loss.mean()
@@ -63,85 +65,117 @@ def backpropagation(loss, optimizer):
     optimizer.step()
 
 def record_episode(model, episode_idx):
-    num_record_per_episode = 1
+    num_record_per_episode = 3
 
     env = gym.make("LunarLander-v2", render_mode="rgb_array")
-    env = RecordVideo(env, video_folder="luna-lander-pg", name_prefix="training-{}".format(episode_idx), 
+    env = RecordVideo(env, video_folder="lunar-lander-pg", name_prefix="training-{}".format(episode_idx), 
                       episode_trigger=lambda x: True)
     env = RecordEpisodeStatistics(env)
+    scores = []
     
     for episode_num in range(num_record_per_episode):
         obs, info = env.reset()
+        score = 0
     
         episode_over = False
         while not episode_over:
             action = select_action(obs, model)
             obs, reward, terminated, truncated, info = env.step(action)
+            score += reward
     
             episode_over = terminated or truncated
+        scores.append(score)
+
     env.close()
+    print("Score evaluated is: {} {}".format(np.mean(scores), scores))
+    print("=" * 80)
 
 # Training Process
-optimizer = optim.Adam(dqn.parameters(), lr=LearningRate)
-scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=NumEpisodes)
+optimizer = optim.Adam(pg.parameters(), lr=LearningRate)
+scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=NumBatches)
 
-# Record for finel evaluation
+# Record for final evaluation
 episode_scores = []
 episode_steps = []
 
 # Train one batch
-for i in tqdm(range(1, NumEpisodes+1)):
+for i in tqdm(range(1, NumBatches+1)):
     # collect episode info
     # reset for one new episode
-    epi_states = []
-    epi_actions = []
-    epi_rewards = []
+    batch_states = []
+    batch_actions = []
+    batch_rewards = []
+    terminate_count = 0
 
-    state, info = env.reset()
-    epi_score = 0
-    epi_step = 0
+    for j in range(EpisodesPerBatch):
+        state, info = env.reset()
+        epi_score = 0
+        epi_step = 0
+        epi_rewards = []
+        epi_states = []
+        epi_actions = []
 
-    # Train one episode
-    while epi_step < MaxEpisodeSteps: 
-        # select an action
-        action = select_action(state, dqn)
-        # receive env update
-        new_state, reward, done, truncated, info = env.step(action)
-        state = new_state
-        epi_step += 1
+        # Train one episode
+        while True: 
+            # select an action
+            action = select_action(state, pg)
+            epi_states.append(state)
+            epi_actions.append(action)
+            # receive env update
+            state, reward, terminated, truncated, info = env.step(action)
+            epi_score += reward
+            epi_rewards.append(reward)
 
-        epi_score += reward
-        epi_states.append(state)
-        epi_actions.append(action)
+            epi_step += 1
 
-        # if episode done, start over
-        if done or epi_step == MaxEpisodeSteps:
-            episode_scores.append(epi_score)
-            episode_steps.append(epi_step)
-            # expand epi_score
-            epi_rewards.extend([epi_score] * epi_step)
+            #print("Truncated") if truncated else None
+            done = terminated or truncated
+            # if episode done, start over
+            if terminated:# Consider Only Complete Trajectories 
+                terminate_count += 1
+                episode_scores.append(epi_score)
+                episode_steps.append(epi_step)
 
-            # calculate loss
-            loss = calculate_loss(dqn, epi_actions, epi_states, epi_rewards)
-            # backpropagation
-            backpropagation(loss, optimizer)
+                batch_states.extend(epi_states)
+                batch_actions.extend(epi_actions)
+                # expand epi_score
+                #epi_rewards.extend([epi_score] * epi_step)
+                # only consider rewards from now on
+                k = epi_step - 2
+                while True:
+                    epi_rewards[k] = epi_rewards[k] + Gamma * epi_rewards[k+1] 
+                    k -= 1
+                    if k < 0:
+                        break
+                batch_rewards.extend(epi_rewards)
 
-            break
+                break
+            elif truncated:
+                break
+
+        if len(batch_actions) == 0:
+            continue
+
+    print("Terminated in Batch: {} of {}".format(terminate_count, EpisodesPerBatch))
+    # calculate loss
+    loss = calculate_loss(pg, batch_actions, batch_states, batch_rewards)
+    # backpropagation
+    backpropagation(loss, optimizer)
 
     scheduler.step()
 
-    if i % 100 == 0:
+    if i % 10 == 0:
         # print info of each batch
         print('-' * 80)
-        print("Score of episode {}: {}".format(i, np.mean(episode_scores[-100:])))
+        print("Score of episode {}: {}".format(i, np.mean(episode_scores[-10 * EpisodesPerBatch:])))
         print("Lr of batch {}: {}".format(i, optimizer.param_groups[0]['lr']))
 
-        record_episode(dqn, i)
+        record_episode(pg, i)
 
 
 #print(episode_scores)
 #print(episode_steps)
-torch.save(dqn.state_dict(), './trained_lunar-pg.pth')
+torch.save(pg.state_dict(), './trained_lunar-pg.pth')
 
 with open('lunar-pg-scores.csv', 'w', newline='') as file:
     writer = csv.writer(file)
